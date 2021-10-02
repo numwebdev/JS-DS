af2323e140661ac31a3d831b40ce28a1624d8b77
Numan Ahmed <mdnmnahmed@gmail.com>
2021-10-02 21:27:55 +0530

    Array Operation - Push & Pop

---
+++ Array/arrayOperations.js
@@ -0,0 +1,19 @@
+const numbers = [1, 3, 8, 2, 5];
+const strings = ['n', 'u', 'm'];
+
+/**
+ * Add element at the End
+ */
+strings.push('a');
+strings.push('n');
+
+
+/**
+ * Remove last element
+ */
+strings.pop();
+
+
+
+
+console.log(strings);
